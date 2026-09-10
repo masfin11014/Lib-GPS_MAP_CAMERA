a9835854d7ebeae20a9002ace8676b5f9ccb7b12
masfin11014 <masappdeveloper@gmail.com>
2026-09-10 17:14:32 +0530

    add flag for photo icon

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -56,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: MapCameraLocation(
+          showPhoto: true,
           showGallery: true,
           showVideo: true,
           camera: widget.camera,
--- lib/src/map_camera.dart
+++ lib/src/map_camera.dart
@@ -17,13 +17,14 @@ class MapCameraLocation extends StatefulWidget {
   final String? btnText;
   final bool? showGallery;
   final bool? showVideo;
+  final bool? showPhoto;
 
   /// Constructs a MapCameraLocation widget.
   ///
   /// The [camera] parameter is required and represents the camera to be used for capturing images.
   /// The [onImageCaptured] parameter is an optional callback function that will be triggered when an image and location data are captured.
   const MapCameraLocation(
-      {super.key, required this.camera,required this.onImageCaptured,required this.onGalleryClick,required this.onVideoClick,required this.btnText,required this.showGallery,required this.showVideo});
+      {super.key, required this.camera,required this.onImageCaptured,required this.onGalleryClick,required this.onVideoClick,required this.btnText,required this.showGallery,required this.showVideo, required this.showPhoto});
 
   @override
   State<MapCameraLocation> createState() => _MapCameraLocationState();
@@ -281,14 +282,14 @@ class _MapCameraLocationState extends State<MapCameraLocation> {
               }
             },
             //child: const Icon(Icons.camera_alt),
-            child: Column(
+            child: (widget.showPhoto) ? Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const Icon(Icons.camera_alt),
                 Text("Photo",style: TextStyle(fontSize: 10),)
               ],
-            ),
+            ): Offstage(),
           ),
           SizedBox(width: 10,),
           if(widget.showVideo!)
